cec3cc80783b40339c90255a4ab07eadf4b3d6f6
lavaloid <36921951+lavaloid@users.noreply.github.com>
2026-02-07 18:06:13 +0700

    add keyboard nav for calendar

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,9 @@ export const range = (from: number, to: number) =>
   Array.from({
     length: to - from + 1,
   }).map((_, i) => i + from);
+
+export const formatISODate = (date: Date) => {
+  // using .toLocaleDateString("sv") will output the date formatted
+  // as ISO 8601. thank you sweden
+  return date.toLocaleDateString("sv");
+};

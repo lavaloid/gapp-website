20abd491ed5524346325743ca6b1960e04eb7f0a
lavaloid <36921951+lavaloid@users.noreply.github.com>
2026-04-19 22:04:24 +0700

    support vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	output: "server",
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: vercel(),
 });

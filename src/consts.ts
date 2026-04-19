a7d2315f12b88968606e92ae5341f800d5d1fe4c
lavaloid <36921951+lavaloid@users.noreply.github.com>
2026-04-19 23:31:37 +0700

    add special handling of posts before channel was created

--- src/consts.ts
+++ src/consts.ts
@@ -3,5 +3,10 @@ export const SITE_DESCRIPTION = "A daily series of pencil puzzles designed to re
 
 export const GAPP_START_DATE = new Date(2021, 9, 27); // 2021 Oct 27
 export const GAPP_PUBLISH_DELAY_DAYS = 7;
+/**
+ * This is the date of the first GAPP post that is posted in #daily-pencil-puzzles, and not
+ * copy-pasted from #other-testing-submissions
+ */
+export const GAPP_CHANNEL_START_DATE = new Date(2021, 10, 21); // 2021 Nov 21
 
 export const RSS_POST_COUNT = 32;
--- src/discord.ts
+++ src/discord.ts
@@ -5,6 +5,7 @@ import {
   TextChannel,
 } from "discord.js";
 import dotenv from "dotenv";
+import { GAPP_CHANNEL_START_DATE, GAPP_START_DATE } from "./consts";
 
 dotenv.config();
 const client = new Client({
@@ -19,25 +20,74 @@ await client.login(process.env.DISCORD_APP_TOKEN);
 export const getClient = () => client;
 
 export const getPostsFromDate = async (date: Date) => {
+  /**
+   * GAPP posts before GAPP_CHANNEL_START_DATE (2021 Nov 21) are posted in
+   * #other-testing-submissions, and they're only moved to #daily-pencil-puzzles
+   * on GAPP_CHANNEL_START_DATE. So we should handle those posts differently
+   */
+
   const client = getClient();
   const channel = (await client.channels.fetch(
     process.env.DISCORD_CHANNEL_ID || "",
     { cache: true },
   )) as TextChannel;
 
-  // We're fetching messages at a certain date by constructing a fake message ID
-  // at a specified date, then querying messages after that.
-  const snowflakeId = SnowflakeUtil.generate({ timestamp: +date });
-  return (
-    await channel.messages.fetch({
-      after: `${snowflakeId}`,
-      cache: true,
-      limit: 10,
-    })
-  )
-    .filter(
-      (message) => message.createdAt <= new Date(+date + 24 * 60 * 60 * 1000),
+  if (date < GAPP_CHANNEL_START_DATE) {
+    /**
+     * Messages before GAPP_CHANNEL_START_DATE follow this pattern:
+     * - 8 informational messages & reserved pins messages from bakpao
+     * - After that every two posts are daily puzzles with this pattern:
+     *    - bakpao: <date>, puzzle by <author>
+     *    - CopynPaste: <the actual puzzle content>
+     */
+    const snowflakeId = SnowflakeUtil.generate({ timestamp: +GAPP_START_DATE });
+    const rawMessages = (
+      await channel.messages.fetch({
+        after: `${snowflakeId}`,
+        cache: true,
+        limit:
+          Math.round(
+            (+GAPP_CHANNEL_START_DATE - +GAPP_START_DATE) /
+              (24 * 60 * 60 * 1000),
+          ) *
+            2 +
+          8,
+      })
     )
-    .map((v) => v)
-    .toReversed();
+      .map((v) => v)
+      .toReversed();
+
+    const dateIndex = Math.round(
+      (+date - +GAPP_START_DATE) / (24 * 60 * 60 * 1000),
+    );
+
+    const metaMessage = rawMessages.at(dateIndex * 2 + 8);
+    const puzzleMessage = rawMessages.at(dateIndex * 2 + 9);
+
+    const authorName = metaMessage?.content.split(" by ").at(1);
+
+    if (!puzzleMessage) return [];
+    return [
+      {
+        ...puzzleMessage,
+        authorOverride: authorName,
+      },
+    ];
+  } else {
+    // We're fetching messages at a certain date by constructing a fake message ID
+    // at a specified date, then querying messages after that.
+    const snowflakeId = SnowflakeUtil.generate({ timestamp: +date });
+    return (
+      await channel.messages.fetch({
+        after: `${snowflakeId}`,
+        cache: true,
+        limit: 10,
+      })
+    )
+      .filter(
+        (message) => message.createdAt <= new Date(+date + 24 * 60 * 60 * 1000),
+      )
+      .map((v) => ({ ...v, authorOverride: undefined })) // typescript nonsense
+      .toReversed();
+  }
 };
